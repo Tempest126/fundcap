4dd5ae8aeebf7505c3601fd3e58ebc599de880f9
Tempest126 <60488553+Tempest126@users.noreply.github.com>
2020-12-01 00:17:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,85 @@
+
+
+$('.icon-menu').click(function (event) {
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	if ($(this).hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	$('body').toggleClass('lock');
+	if (!$(this).hasClass('active')) {
+		$('body,html').scrollTop(parseInt($('body').data('scroll')));
+	}
+});
+
+
+
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
+
+
+function fontsStyle(params) {
+
+	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+	if (file_content == '') {
+		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+		return fs.readdir(path.build.fonts, function (err, items) {
+			if (items) {
+				let c_fontname;
+				for (var i = 0; i < items.length; i++) {
+					let fontname = items[i].split('.');
+					fontname = fontname[0];
+					if (c_fontname != fontname) {
+						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+					}
+					c_fontname = fontname;
+				}
+			}
+		})
+	}
+}
+
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+
+// window.addEventListener('scroll', function () {
+// 	let h = -pageYOffset / 7 + 'px';
+// 	elem = document.querySelector('.about__bg');
+// 	elem.style.transform = `translateY(${h})`;
+// });
+
+
+// function heightStages() {
+
+// 	if (document.documentElement.clientWidth < 768) {
+// 		let h = document.querySelector('.stages').offsetHeight;
+// 		let funds = document.querySelector('.funds');
+// 		funds.style.paddingTop = h + 150 + "px";
+// 	}
+
+
+// }
+// heightStages()
+
+
